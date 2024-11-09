9d794520268ec4d2ec410013c23573f93d408831
nabiilahaulasafda <nabiilahaulasafdaa@gmail.com>
2024-11-09 10:35:25 +0700

    membuat proses

---
+++ proses.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="row mt-5">
+      <div class="col-6 m-auto">
+        <div class="card">
+          <div class="card-header">
+            <h3> Formulir Dosen </h3>
+          </div>
+
+          <div class="card-body">
+            <table class="table">
+
+              <tbody>
+                <tr>
+                  <td scope="row"> NIDN </td>
+                  <th>: <?= $nidn ?> </th>
+                </tr>
+                <tr>
+                  <td scope="row"> Nama Lengkap </td>
+                  <th>: <?= $nama ?> </th>
+                </tr>
+                <tr>
+                  <td scope="row"> Rumpun </td>
+                  <th>: <?= $rumpun ?> </th>
+                </tr>
+                <tr>
+                  <td scope="row"> Tempat Lahir </td>
+                  <th>: <?= $tempat ?> </th>
+                </tr>
+                <tr>
+                  <td scope="row"> Tanggal Lahir </td>
+                  <th>: <?= $tanggal ?> </th>
+                </tr>
+                <tr>
+                  <td scope="row"> Email </td>
+                  <th>: <?= $agama ?> </th>
+                </tr>
+
+              </tbody>
+
+            </table>
+            <a href="index.php" class="btn btn-warning btn-sm"> Back </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="js/bootstrap.js"></script>
+  <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
