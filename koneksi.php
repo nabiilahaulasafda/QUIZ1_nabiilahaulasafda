0938efe662fc5df7f167893c572f1591ab1f41b6
nabiilahaulasafda <nabiilahaulasafdaa@gmail.com>
2024-11-09 10:47:28 +0700

    membuat koneksi

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost" ;
+$username = "root" ;
+$password = ""  ;
+$database = "QUIZ1_nabiilahaulasafda" ;
+
+
+$koneksi = mysqli_connect($host,$username,$password,$database) ;
+
+
+?>
