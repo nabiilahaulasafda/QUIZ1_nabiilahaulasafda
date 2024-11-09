dc10d5c3083ea3123f165d6b84d3dc6a1c78ddc3
nabiilahaulasafda <nabiilahaulasafdaa@gmail.com>
2024-11-09 10:50:58 +0700

    menambahkan link

--- proses.php
+++ proses.php
@@ -23,6 +23,7 @@ mysqli_query($koneksi, $simpan);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" class="css/bootstrap.css" >
 </head>
 <body>
 <div class="container">
